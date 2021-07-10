bb268fece7a9c2bd6f4fb5be8d7d4c0b17fa98c4
Zdeněk Gold <zigi@zdenek-gold.cz>
2021-07-11 00:01:38 +0200

    init

---
+++ src/main/java/com/example/demo/MainView.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+    public MainView() {
+        add(new Text("Welcome to MainView."));
+    }
+}
